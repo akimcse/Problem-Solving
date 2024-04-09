19a438ccaf61889a909a806baeee8f9f2167008f
hyuna <akxmcse@gmail.com>
2024-04-09 16:40:03 +0900

    240409_ kangaroo

---
+++ week3/240409_ kangaroo.cpp
@@ -0,0 +1,119 @@
+// 240409
+// https://www.hackerrank.com/challenges/kangaroo
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'kangaroo' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER v1
+ *  3. INTEGER x2
+ *  4. INTEGER v2
+ */
+
+string kangaroo(int x1, int v1, int x2, int v2) {
+    string posiblity;
+    int kangaroo1 = x1;
+    int kangaroo2 = x2;
+    
+    // 속력이 한쪽이 더 빠르다는 전제 하에 따라잡을 때까지 무한 반복
+    if(v1 > v2){
+        if(kangaroo1 > kangaroo2) posiblity = "NO"; // 속력도 느린데 시작점까지 뒤쳐졌다면 애초에 같아지는 건 불가능
+        while (kangaroo1 < kangaroo2){
+            kangaroo1 += v1;
+            kangaroo2 += v2;
+            if(kangaroo1 == kangaroo2) {
+                posiblity = "YES";
+                break;
+            }
+            posiblity = "NO";
+        }
+    }else {
+        if(kangaroo1 < kangaroo2) posiblity = "NO";
+        while (kangaroo1 > kangaroo2){
+            kangaroo1 += v1;
+            kangaroo2 += v2;
+            if(kangaroo1 == kangaroo2) {
+                posiblity = "YES";
+                break;
+            }
+            posiblity = "NO";
+        }
+    }
+    
+    return posiblity;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int x1 = stoi(first_multiple_input[0]);
+
+    int v1 = stoi(first_multiple_input[1]);
+
+    int x2 = stoi(first_multiple_input[2]);
+
+    int v2 = stoi(first_multiple_input[3]);
+
+    string result = kangaroo(x1, v1, x2, v2);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
